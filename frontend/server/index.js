9ab06c32ecb6a4c498a39ecbf01a4c55c8a28a85
Amy-Kate Andrews <amy.kate.andrews@gmail.com>
2017-05-06 20:24:11 +0000

    live changes

--- frontend/server/index.js
+++ frontend/server/index.js
@@ -3,7 +3,8 @@
 const app = require('./app');
 
 const PORT = process.env.PORT || 9000;
+const IP = '107.170.246.10'
 
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`);
+app.listen(PORT, IP, () => {
+  console.log(`Server running at http://${IP}:${PORT}`);
 });
